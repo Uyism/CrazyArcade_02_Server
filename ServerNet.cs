7c3e741058239e9278bdc95c3c95259a683f6c15
Uyism <60610549+Uyism@users.noreply.github.com>
2020-05-04 02:12:51 +0900

    UDP

--- ServerNet.cs
+++ ServerNet.cs
@@ -26,15 +26,18 @@ public class ServerNet
 
     List<Socket> mClinetSocketList;
     CrazyArcadeRequestHandler mRequestHandler;
+    EndPoint mRemoteEndPoint;
 
+    bool mIsUdp = true;
     public ServerNet()
     {
         mRequestHandler = new CrazyArcadeRequestHandler();
         mRequestHandler.InitCrazyArcadeHandler();
 
         // 1. 서버 소켓 준비
-        StartServerSocket();
+        //StartServerSocket();
 
+        StartUDPServerSocket();
     }
 
     void StartServerSocket()
@@ -57,6 +60,30 @@ public class ServerNet
         }
     }
 
+    void StartUDPServerSocket()
+    {
+        mIsUdp = true;
+
+        IPEndPoint ip_point = new IPEndPoint(IPAddress.Any, 8888);
+        mRemoteEndPoint = new IPEndPoint(IPAddress.None, 9992);
+
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        socket.Bind(ip_point);
+        mClinetSocketList = new List<Socket>();
+        Console.WriteLine("Socket Connect");
+
+
+
+           // socket.Listen(100);
+            //Socket client_socket = socket.Accept();
+
+            // 소켓 연결되면 쓰레드에서 실행 시킴
+            Thread client_thread = new Thread(() => RunClient(ref socket));
+            mClinetSocketList.Add(socket);
+            client_thread.Start();
+
+    }
+
     void RunClient(ref Socket socket)
     {
 
@@ -67,17 +94,17 @@ public class ServerNet
             // 1. 통신을 받는다
             string recieve_msg = Receive(ref socket);
             if (recieve_msg == ErrorCode.Error.ToString())
-                return;
+                continue;
 
             // 2. request에 알맞는 response를 만든다
             StructRequest response = MakeResponse(ref socket, recieve_msg);
             if (IsErrorStructRequest(response))
-                return;
+                continue;
 
             // 3. response 전달
             bool is_success = Send(ref socket, response);
             if (!is_success)
-                return;
+                continue;
 
         }
         socket.Close();
@@ -88,7 +115,10 @@ public class ServerNet
         try
         {
             Byte[] data = new Byte[BYTESIZE];
-            socket.Receive(data);
+            if (!mIsUdp)
+                socket.Receive(data);
+            else
+                socket.ReceiveFrom(data, ref mRemoteEndPoint);
             String msg = Encoding.Default.GetString(data);
             Console.WriteLine("수신 : " + msg);
             return msg;
@@ -110,7 +140,12 @@ public class ServerNet
         try
         {
             Console.WriteLine("송신 : " + send);
-            socket.Send(_data);
+
+            if (!mIsUdp)
+                socket.Send(_data);
+            else
+                socket.SendTo(_data, mRemoteEndPoint);
+
             return true;
 
         }
